caf9bf3f928e24757c87310994329af817852b4d
fabianmedrano <135065707+fabianmedrano@users.noreply.github.com>
2023-07-25 13:53:52 -0600

    Agregar archivos de proyecto.

---
+++ Character.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prototype
+{
+    public class Character
+    {
+        private int health;
+        private int strength;
+        private int defence;
+
+        public int Health { get => health; set => health = value; }
+        public int Strength { get => strength; set => strength = value; }
+        public int Defence { get => defence; set => defence = value; }
+
+        public Character Clone() {
+            return (Character)this.MemberwiseClone();
+        } 
+        public override string ToString()
+        {
+            return $"health {health}, Strength {strength}, defence {defence}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Character originalCharacter = new Character();
+            originalCharacter.Health = 100;
+            originalCharacter.Strength = 150;
+            originalCharacter.Defence = 200;
+
+            Character clone = originalCharacter.Clone();
+            clone.Defence = 600;
+            clone.Health = 60;
+
+            Console.WriteLine(originalCharacter);
+            Console.WriteLine(clone);
+
+        }
+    }
+}
